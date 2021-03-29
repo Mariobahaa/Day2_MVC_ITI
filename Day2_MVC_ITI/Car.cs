7448aae0951ebd85674bd6a78284a46111c15e2b
MarioBahaa <32718005+Mariobahaa@users.noreply.github.com>
2021-03-29 02:52:20 +0200

    Initial commit

---
+++ Day2_MVC_ITI/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Day2_MVC_ITI
+{
+    public class Car
+    {
+        public int Num { get; set; }
+        public string Color { get; set; }
+        public string Model { get; set; }
+        public string Manufacturer { get; set; }
+
+        public Car() { }
+        public Car(int num, string col, string mod, string man) 
+        {
+            Num = num; Color = col; Model = mod; Manufacturer = man;
+        }
+
+
+    }
+}
---
+++ Day2_MVC_ITI/Controllers/CarController.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Day2_MVC_ITI.Controllers
+{
+    public class CarController : Controller
+    {
+        static List<Car> cars = new List<Car>() {
+
+          new Car(1, "red", "GT", "Ferrari"),
+          new Car(2, "black", "Veyron", "Bugatti"),
+          new Car(3, "silver", "Aventador", "Lamborghini"),
+          new Car(4, "yellow", "Camaro SS", "Chevrolet")
+        };
+        // GET: Car
+        public ActionResult GetAllCars()
+        {
+            ViewBag.carList = cars;
+            return View();
+        }
+
+        public ActionResult GetCarById(int id)
+        {
+            ViewBag.car = cars.Where(C => C.Num == id).FirstOrDefault();
+            return View();
+        }
+
+     
+        public ActionResult Update(int id)
+        {
+            ViewBag.car = cars.Where(C => C.Num == id).FirstOrDefault();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Update(int id, string color, string model, string manufacturer)
+        {
+            Car updatedCar = cars.Where(C => C.Num == id).FirstOrDefault();
+            updatedCar.Num = id;
+            updatedCar.Color = color;
+            updatedCar.Model = model;
+            updatedCar.Manufacturer = manufacturer;
+            return RedirectToAction("GetAllCars");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            cars.Remove(cars.Where(C => C.Num == id).FirstOrDefault());
+
+            return RedirectToAction("GetAllCars");
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Add(int id, string color, string model, string manufacturer)
+        {
+            Car newCar = new Car(id,color,model,manufacturer);
+            cars.Add(newCar);
+
+            return RedirectToAction("GetAllCars");
+        }
+    }
+}
